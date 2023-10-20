05a75027f6662c0b6b7573b25043ca2284b937ea
Gabrielle <gabrielleajornalist@gmailcom>
2023-10-20 19:41:27 -0300

    Front End

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,29 +5,31 @@ function Home() {
   const [persons, setPersons] = useState([]);
   const [nome, setNome] = useState("");
   const [numero, setNumero] = useState("");
+  const [showForm, setShowForm] = useState(false);
 
   useEffect(() => {
     fetchData(); //Carrega os dados iniciais
-   },[]);
+  }, []);
 
-  const fetchData = () => {
-    personService.getAll().then((response) => {
-      setPersons(response.data);  
-  })
-  }; 
-    const addPerson = async (event) => {
-      event.preventDefault();
+  const fetchData = async () => {
+   await personService.getAll().then((response) => {
+      setPersons(response.data);
+      setShowForm(false);
+    });
+  };
+  const addPerson = async (event) => {
+    event.preventDefault();
     const personObject = {
       nome: nome,
       numero: numero,
-    }
-    
+    };
+
     await personService.create(personObject);
-   // console.log("button clicked", event.target);
-   setNome("");
-   setNumero("");
-   // Após criação, atualize a lista de persons chamando fetchData novamente
-   fetchData();
+    // console.log("button clicked", event.target);
+    setNome("");
+    setNumero("");
+    // Após criação, atualize a lista de persons chamando fetchData novamente
+    fetchData();
   };
 
   const handleNomeChange = (event) => {
@@ -40,56 +42,67 @@ function Home() {
     setNumero(event.target.value);
   };
 
+  const toggleForm = () => {
+    setShowForm(!showForm);
+  };
+
   return (
     <div className="container">
       <h2>Home</h2>
-      <form onSubmit={addPerson}>
-        <div className="mb-3">
-          <label htmlFor="nome" className="form-label">
-            Nome:
-          </label>
-          <input
-            type="text"
-            placeholder="Digite o seu nome..."
-            className="form-control"
-            onChange={handleNomeChange}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="telefone" className="form-label">
-            Telefone:
-          </label>
-          <input
-            type="text"
-            placeholder="Digite o seu telefone..."
-            className="form-control"
-            onChange={handleNumeroChange}
-          />
-          <button className="btn btn-success mt-4">Cadastrar</button>
-        </div>
-      </form>
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Nome</th>
-            <th scope="col">Telefone</th>
-            <th scope="col">Ações</th>
-          </tr>
-        </thead>
-        <tbody>
-          {persons.map((person) => (
-            <tr key={person.id}>
-              <td>{person.id}</td>
-              <td>{person.nome}</td>
-              <td>{person.numero}</td>
-              <td></td>
+      <button onClick={toggleForm} className="btn btn-success">
+        {showForm ? "Voltar para a Tabela " : "Cadastrar Pessoa"}
+      </button>
+
+      {showForm ? (
+        <form onSubmit={addPerson}>
+          <div className="mb-3">
+            <label htmlFor="nome" className="form-label">
+              Nome:
+            </label>
+            <input
+              type="text"
+              placeholder="Digite o seu nome..."
+              className="form-control"
+              onChange={handleNomeChange}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="telefone" className="form-label">
+              Telefone:
+            </label>
+            <input
+              type="text"
+              placeholder="Digite o seu telefone..."
+              className="form-control"
+              onChange={handleNumeroChange}
+            />
+            <button className="btn btn-success mt-4">Cadastrar</button>
+          </div>
+        </form>
+      ) : (
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Nome</th>
+              <th scope="col">Telefone</th>
+              <th scope="col">Ações</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {persons.map((person) => (
+              <tr key={person.id}>
+                <td>{person.id}</td>
+                <td>{person.nome}</td>
+                <td>{person.numero}</td>
+                <td></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
-          }
+}
 
 export default Home;
